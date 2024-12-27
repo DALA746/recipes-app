1d5cf4b8d55aced59c02763cff33ca80d94609ae
DALA746 <darya.lapata@gmail.com>
2024-12-27 18:32:15 +0100

    laying the ground, sign in form, login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,9 @@ import logo from '../images/popcorn.png';
 
 const Header = () => {
   return (
-    <div className="w-full p-3">
+    <div className="w-full p-3 flex items-center gap-2">
       <img className="w-16" src={logo} alt="logo" />
+      <span className="text-white font-bold">POPFLIX</span>
     </div>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
-// import background from '../images/background.jpg';
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
-    <div className="bg-black h-full">
+    <div className="bg-gradient-to-r from-purple-500 to-pink-500 h-screen w-full">
       <Header />
-      {/* <img className="w-full h-full" src={background} alt="background" /> */}
-      <form>
-        <input
-          className="p-2 max-w-screen-2xl"
-          type="text"
-          placeholder="Email"
-        />
-        <input
-          className="p-2 max-w-screen-2xl"
-          type="text"
-          placeholder="Password"
-        />
-        <button className="p-4 m-4">Sign in</button>
-      </form>
+      <div className="flex items-center justify-center bg-opacity-15">
+        <form className="bg-black bg-opacity-10 w-1/4 p-3 flex flex-col justify-center items-center gap-2">
+          <h1 className="text-start text-3xl font-bold text-white">
+            {isSignInForm ? 'Sign in' : 'Sign up'}
+          </h1>
+          {!isSignInForm && (
+            <input className="p-2 w-full" type="text" placeholder="Username" />
+          )}
+
+          <input className="p-2 w-full" type="text" placeholder="Email" />
+          <input className="p-2 w-full" type="text" placeholder="Password" />
+          <button className="p-2 m-2 bg-red-600 rounded w-full text-white">
+            {isSignInForm ? 'Sign in' : 'Sign up'}
+          </button>
+          <h2
+            className="text-white text-center font-bold cursor-pointer"
+            onClick={toggleSignInForm}>
+            {isSignInForm
+              ? 'Are you new to Netflix? Sign up now!'
+              : 'Already a reistered? Sign in!'}
+          </h2>
+        </form>
+      </div>
     </div>
   );
 };
